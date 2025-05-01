b859178df5f25d49cb795e223570d581ba791029
Javier Mora <javier23@correo.ugr.es>
2025-05-01 12:57:55 +0200

    Añadir P4: ejemplos

---
+++ P4/Ejemplo1/Ejemplo_I.java
@@ -0,0 +1,6 @@
+// Define Interfaz remota
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public interface Ejemplo_I extends Remote {
+    public void escribir_mensaje (int id_proceso) throws RemoteException;
+}
